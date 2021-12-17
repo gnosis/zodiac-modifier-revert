b808d49bc0a1d435da00931da00bb783bd60b42e
Cristóvão <cristovao.honorato@gmail.com>
2021-12-17 17:56:04 +0100

    Don't use masterCopy deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,32 +1,40 @@
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "solidity-coverage";
-import "hardhat-deploy";
-import "hardhat-gas-reporter";
-import "hardhat-contract-sizer";
-import dotenv from "dotenv";
-import type { HttpNetworkUserConfig } from "hardhat/types";
-import yargs from "yargs";
-import "./src/tasks/setup";
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-waffle';
+import 'solidity-coverage';
+import 'hardhat-deploy';
+import 'hardhat-gas-reporter';
+import 'hardhat-contract-sizer';
+import dotenv from 'dotenv';
+import type { HttpNetworkUserConfig } from 'hardhat/types';
+import yargs from 'yargs';
+import './src/tasks/setup';
 
 const argv = yargs
-  .option("network", {
-    type: "string",
-    default: "hardhat",
+  .option('network', {
+    type: 'string',
+    default: 'hardhat',
   })
   .help(false)
   .version(false).argv;
 
 // Load environment variables.
 dotenv.config();
-const { INFURA_KEY, MNEMONIC, ETHERSCAN_API_KEY, PK } = process.env;
+const {
+  INFURA_KEY,
+  MNEMONIC,
+  ETHERSCAN_API_KEY,
+  PK,
+  OWNER_ADDRESS,
+  TARGET_ADDRESS,
+  AVATAR_ADDRESS,
+} = process.env;
 
 const DEFAULT_MNEMONIC =
-  "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+  'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat';
 
 const sharedNetworkConfig: HttpNetworkUserConfig = {};
 if (PK) {
-  console.log("yah");
+  console.log('yah');
   sharedNetworkConfig.accounts = [PK];
 } else {
   sharedNetworkConfig.accounts = {
@@ -34,7 +42,7 @@ if (PK) {
   };
 }
 
-if (["rinkeby", "mainnet"].includes(argv.network) && INFURA_KEY === undefined) {
+if (['rinkeby', 'mainnet'].includes(argv.network) && INFURA_KEY === undefined) {
   throw new Error(
     `Could not find Infura key in env, unable to connect to network ${argv.network}`
   );
@@ -42,13 +50,13 @@ if (["rinkeby", "mainnet"].includes(argv.network) && INFURA_KEY === undefined) {
 
 export default {
   paths: {
-    artifacts: "build/artifacts",
-    cache: "build/cache",
-    deploy: "src/deploy",
-    sources: "contracts",
+    artifacts: 'build/artifacts',
+    cache: 'build/cache',
+    deploy: 'src/deploy',
+    sources: 'contracts',
   },
   solidity: {
-    compilers: [{ version: "0.8.6" }, { version: "0.6.12" }],
+    compilers: [{ version: '0.8.6' }, { version: '0.6.12' }],
     settings: {
       optimizer: {
         enabled: true,
@@ -70,15 +78,18 @@ export default {
     },
     xdai: {
       ...sharedNetworkConfig,
-      url: "https://xdai.poanetwork.dev",
+      url: 'https://xdai.poanetwork.dev',
     },
     matic: {
       ...sharedNetworkConfig,
-      url: "https://rpc-mainnet.maticvigil.com",
+      url: 'https://rpc-mainnet.maticvigil.com',
     },
   },
   namedAccounts: {
     deployer: 0,
+    owner: OWNER_ADDRESS,
+    target: TARGET_ADDRESS,
+    avatar: AVATAR_ADDRESS,
   },
   mocha: {
     timeout: 2000000,
--- src/deploy/deploy_module.ts
+++ src/deploy/deploy_module.ts
@@ -5,23 +5,14 @@ const FirstAddress = '0x0000000000000000000000000000000000000001';
 
 const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
-  const { deployer } = await getNamedAccounts();
+  const { deployer, owner, target, avatar } = await getNamedAccounts();
   const { deploy } = deployments;
-  const args = [FirstAddress, FirstAddress, FirstAddress];
-
-  const txCheck = await deploy('TransactionCheck', {
-    from: deployer,
-    log: true,
-  });
+  const args = [owner, target, avatar];
 
   await deploy('Revert', {
     from: deployer,
     args,
     log: true,
-    deterministicDeployment: true,
-    libraries: {
-      TransactionCheck: txCheck.address,
-    },
   });
 };
 
